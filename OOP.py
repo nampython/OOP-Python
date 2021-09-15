5e2eabb3098fc4e31f7408fc4149a00cfaa129cf
NamQuang13 <dragonsang10a1@gmail.com>
2021-09-15 22:29:54 +0700

    OOP PYTHON

---
+++ OOP.py
@@ -0,0 +1,12 @@
+class Person():
+    def __init__(self, name, age, address, phone):
+        self.name = name
+        self.age = age
+        self.address = address
+        self.phone = phone
+
+
+
+
+
+if __name__ == '__main__':
